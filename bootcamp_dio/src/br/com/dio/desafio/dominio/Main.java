c7f51fbb840693c306916868b91c0a5866b4da5a
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2023-08-01 15:44:22 -0300

    creat class mentoria, curso and main

---
+++ bootcamp_dio/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+	public static void main(String[] args) {
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Java");
+		curso1.setDescricao("Descricao Java");
+		curso1.setCargaHoraria(3);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("JavaScript");
+		curso2.setDescricao("Descricao JavaScript");
+		curso2.setCargaHoraria(89);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria Java");
+		mentoria.setDescricao("Descrição mentoria Java");
+		mentoria.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
+	}
+}
